9fc099fc356bdfed124454567afb94604d30e93b
AGagnon <agagnon@cegepba.qc.ca>
2025-01-27 16:31:26 -0500

    retrait de code

--- PO1_AssuranceAuto/Form1.cs
+++ PO1_AssuranceAuto/Form1.cs
@@ -11,40 +11,7 @@ namespace PO1_AssuranceAuto
 
         private void button_CalculerPrime_Click(object sender, EventArgs e)
         {
-            bool parseValeurOk = float.TryParse(textBox_valeur.Text, out float valeur);
-            if(!parseValeurOk)
-            {
-                MessageBox.Show("La valeur n'est pas un nombre valide");
-                return;
-            }
-            parseValeurOk =  int.TryParse(textBox_anneesExperience.Text, out int nbAnneesExperience);
-            if (!parseValeurOk)
-            {
-                MessageBox.Show("Le nombre d'années d'expérience n'est pas un nombre valide");
-                return;
-            }
-            parseValeurOk =  int.TryParse(textBox_accidentResponsable.Text, out int nbAccidentsResponsables);
-            if (!parseValeurOk)
-            {
-                MessageBox.Show("Le nombre d'accidents responsables n'est pas un nombre valide");
-                return;
-            }
-
-           
-
-            float prime = 250 + 0.05f * valeur;
-           
-            
-            if(nbAnneesExperience < surprimeFaibleExpérience.Length)
-            {
-                prime = prime * (1+surprimeFaibleExpérience[nbAnneesExperience]);
-            }
-
-            if(nbAccidentsResponsables > 0)
-            {
-                prime = (float)(prime * Math.Pow(1 + 0.25d , nbAccidentsResponsables));
-            }
-            MessageBox.Show("La prime est de " + prime + "$");
+          
         }
     }
 }
