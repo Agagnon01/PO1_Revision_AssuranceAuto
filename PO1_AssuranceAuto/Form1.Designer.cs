b46fc09d4a148bae14c0191d1399877067796596
AGagnon <agagnon@cegepba.qc.ca>
2025-01-27 16:16:41 -0500

    Simplification de l exercice

--- PO1_AssuranceAuto/Form1.Designer.cs
+++ PO1_AssuranceAuto/Form1.Designer.cs
@@ -32,13 +32,8 @@
             textBox_anneesExperience = new TextBox();
             label_valeur = new Label();
             label_accidentResponsable = new Label();
-            label_pointsInaptitudePerdus = new Label();
-            radioButton_creditBon = new RadioButton();
             textBox_valeur = new TextBox();
             textBox_accidentResponsable = new TextBox();
-            textBox_pointsInaptitudePerdus = new TextBox();
-            radioButton_creditMediocre = new RadioButton();
-            label_credit = new Label();
             button_CalculerPrime = new Button();
             SuspendLayout();
             // 
@@ -76,26 +71,6 @@
             label_accidentResponsable.TabIndex = 3;
             label_accidentResponsable.Text = "Nombre d'accident responsable";
             // 
-            // label_pointsInaptitudePerdus
-            // 
-            label_pointsInaptitudePerdus.AutoSize = true;
-            label_pointsInaptitudePerdus.Location = new Point(12, 108);
-            label_pointsInaptitudePerdus.Name = "label_pointsInaptitudePerdus";
-            label_pointsInaptitudePerdus.Size = new Size(257, 20);
-            label_pointsInaptitudePerdus.TabIndex = 5;
-            label_pointsInaptitudePerdus.Text = "Nombre de point d'inaptitude perdus";
-            // 
-            // radioButton_creditBon
-            // 
-            radioButton_creditBon.AutoSize = true;
-            radioButton_creditBon.Location = new Point(309, 138);
-            radioButton_creditBon.Name = "radioButton_creditBon";
-            radioButton_creditBon.Size = new Size(56, 24);
-            radioButton_creditBon.TabIndex = 6;
-            radioButton_creditBon.TabStop = true;
-            radioButton_creditBon.Text = "Bon";
-            radioButton_creditBon.UseVisualStyleBackColor = true;
-            // 
             // textBox_valeur
             // 
             textBox_valeur.Location = new Point(309, 6);
@@ -110,36 +85,9 @@
             textBox_accidentResponsable.Size = new Size(125, 27);
             textBox_accidentResponsable.TabIndex = 8;
             // 
-            // textBox_pointsInaptitudePerdus
-            // 
-            textBox_pointsInaptitudePerdus.Location = new Point(309, 105);
-            textBox_pointsInaptitudePerdus.Name = "textBox_pointsInaptitudePerdus";
-            textBox_pointsInaptitudePerdus.Size = new Size(125, 27);
-            textBox_pointsInaptitudePerdus.TabIndex = 9;
-            // 
-            // radioButton_creditMediocre
-            // 
-            radioButton_creditMediocre.AutoSize = true;
-            radioButton_creditMediocre.Location = new Point(386, 138);
-            radioButton_creditMediocre.Name = "radioButton_creditMediocre";
-            radioButton_creditMediocre.Size = new Size(93, 24);
-            radioButton_creditMediocre.TabIndex = 11;
-            radioButton_creditMediocre.TabStop = true;
-            radioButton_creditMediocre.Text = "Médiocre";
-            radioButton_creditMediocre.UseVisualStyleBackColor = true;
-            // 
-            // label_credit
-            // 
-            label_credit.AutoSize = true;
-            label_credit.Location = new Point(13, 140);
-            label_credit.Name = "label_credit";
-            label_credit.Size = new Size(121, 20);
-            label_credit.TabIndex = 12;
-            label_credit.Text = "Dossier de crédit";
-            // 
             // button_CalculerPrime
             // 
-            button_CalculerPrime.Location = new Point(13, 181);
+            button_CalculerPrime.Location = new Point(13, 111);
             button_CalculerPrime.Name = "button_CalculerPrime";
             button_CalculerPrime.Size = new Size(156, 29);
             button_CalculerPrime.TabIndex = 13;
@@ -153,13 +101,8 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Controls.Add(button_CalculerPrime);
-            Controls.Add(label_credit);
-            Controls.Add(radioButton_creditMediocre);
-            Controls.Add(textBox_pointsInaptitudePerdus);
             Controls.Add(textBox_accidentResponsable);
             Controls.Add(textBox_valeur);
-            Controls.Add(radioButton_creditBon);
-            Controls.Add(label_pointsInaptitudePerdus);
             Controls.Add(label_accidentResponsable);
             Controls.Add(label_valeur);
             Controls.Add(textBox_anneesExperience);
@@ -176,13 +119,8 @@
         private TextBox textBox_anneesExperience;
         private Label label_valeur;
         private Label label_accidentResponsable;
-        private Label label_pointsInaptitudePerdus;
-        private RadioButton radioButton_creditBon;
         private TextBox textBox_valeur;
         private TextBox textBox_accidentResponsable;
-        private TextBox textBox_pointsInaptitudePerdus;
-        private RadioButton radioButton_creditMediocre;
-        private Label label_credit;
         private Button button_CalculerPrime;
     }
 }
--- PO1_AssuranceAuto/Form1.cs
+++ PO1_AssuranceAuto/Form1.cs
@@ -29,23 +29,11 @@ namespace PO1_AssuranceAuto
                 MessageBox.Show("Le nombre d'accidents responsables n'est pas un nombre valide");
                 return;
             }
-            parseValeurOk = int.TryParse(textBox_pointsInaptitudePerdus.Text, out int nbPointsInaptitudePerdus);
-            if (!parseValeurOk)
-            {
-                MessageBox.Show("Le nombre de points d'inaptitude perdus n'est pas un nombre valide");
-                return;
-            }
-            if(!radioButton_creditBon.Checked && !radioButton_creditMediocre.Checked)
-            {
-                MessageBox.Show("Veuillez sélectionner un type de crédit");
-                return;
-            }
+
+           
 
             float prime = 250 + 0.05f * valeur;
-            if (radioButton_creditMediocre.Checked)
-            {
-                prime = 1.25f * prime;
-            }
+           
             
             if(nbAnneesExperience < surprimeFaibleExpérience.Length)
             {
@@ -56,12 +44,6 @@ namespace PO1_AssuranceAuto
             {
                 prime = (float)(prime * Math.Pow(1 + 0.25d , nbAccidentsResponsables));
             }
-
-            if(nbPointsInaptitudePerdus > 0)
-            {
-                prime = (float)(prime * Math.Pow(1 + 0.03d, nbPointsInaptitudePerdus));
-            }
-
             MessageBox.Show("La prime est de " + prime + "$");
         }
     }
